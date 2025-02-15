fd2b2ddc9e4c3f0a8a5773cd81bd52b179a96207
J-Luckas <jairoluckas@hotmail.com>
2025-02-15 12:05:41 -0300

    feat: add user model

--- app/model/user.rb
+++ app/model/user.rb
@@ -1,7 +1,9 @@
-class User 
+# frozen_string_literal: true
+
+class User
   attr_accessor :name, :spend_by_day
 
-  def initialize ( name:, spend_by_day: )
+  def initialize(name:, spend_by_day:)
     @name = name
     @spend_by_day = spend_by_day
   end
