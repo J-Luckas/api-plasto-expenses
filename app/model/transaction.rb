7096e608826f7eff9863fd8b8b19c91d828de2ff
J-Luckas <jairoluckas@hotmail.com>
2025-02-15 12:05:50 -0300

    feat: add transaction model

--- app/model/transaction.rb
+++ app/model/transaction.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Transaction
   attr_accessor :description, :value, :type
 
-  def initialize ( description:, value:, type: )
+  def initialize(description:, value:, type:)
     @description = description
     @value = value
     @type = type
